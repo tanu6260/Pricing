2e3baceda1eccb6e3c605cf51495eb0763d53cce
tanu6260 <yedetanu@gmail.com>
2024-12-14 18:12:49 +0530

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Pricing from "./components/Pricing";
+
+function App() {
+  return (
+    <div className="App">
+      <Pricing />
+    </div>
+  );
+}
+
+export default App;
